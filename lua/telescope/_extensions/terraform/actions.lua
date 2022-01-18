f62b5e510c95ed3f06ce91a00035a0df86792791
Sergey Bulavintsev <bulavintsev.sergey@gmail.com>
2022-01-18 18:16:22 +0300

    refactor: extract nvim_buf_configure

--- lua/telescope/_extensions/terraform/actions.lua
+++ lua/telescope/_extensions/terraform/actions.lua
@@ -5,6 +5,19 @@ local Job = require "plenary.job"
 
 local A = {}
 
+local function nvim_buf_configure(buf, opts)
+  vim.api.nvim_buf_set_name(0, "result #" .. buf)
+
+  vim.api.nvim_buf_set_option(0, "buftype", "nofile")
+  vim.api.nvim_buf_set_option(0, "swapfile", false)
+  vim.api.nvim_buf_set_option(0, "filetype", opts.filetype)
+  vim.api.nvim_buf_set_option(0, "bufhidden", "wipe")
+  vim.api.nvim_command("setlocal " .. opts.wrap)
+  vim.api.nvim_command "setlocal cursorline"
+
+  vim.api.nvim_buf_set_lines(buf, 0, -1, false, { "Retrieving log, please wait..." })
+end
+
 A.append_resource_addr = function(prompt_bufnr)
   local selection = action_state.get_selected_entry()
   actions.close(prompt_bufnr)
@@ -26,16 +39,7 @@ A.apply = function(opts)
     vim.api.nvim_command(opts.wincmd)
     local buf = vim.api.nvim_get_current_buf()
 
-    vim.api.nvim_buf_set_name(0, "result #" .. buf)
-
-    vim.api.nvim_buf_set_option(0, "buftype", "nofile")
-    vim.api.nvim_buf_set_option(0, "swapfile", false)
-    vim.api.nvim_buf_set_option(0, "filetype", opts.filetype)
-    vim.api.nvim_buf_set_option(0, "bufhidden", "wipe")
-    vim.api.nvim_command("setlocal " .. opts.wrap)
-    vim.api.nvim_command "setlocal cursorline"
-
-    vim.api.nvim_buf_set_lines(buf, 0, -1, false, { "Retrieving log, please wait..." })
+    nvim_buf_configure(buf, opts)
 
     local on_output = function(_, line)
       table.insert(apply_output, line)
@@ -83,16 +87,59 @@ A.destroy = function(opts)
     vim.api.nvim_command(opts.wincmd)
     local buf = vim.api.nvim_get_current_buf()
 
-    vim.api.nvim_buf_set_name(0, "result #" .. buf)
+    nvim_buf_configure(buf, opts)
 
-    vim.api.nvim_buf_set_option(0, "buftype", "nofile")
-    vim.api.nvim_buf_set_option(0, "swapfile", false)
-    vim.api.nvim_buf_set_option(0, "filetype", opts.filetype)
-    vim.api.nvim_buf_set_option(0, "bufhidden", "wipe")
-    vim.api.nvim_command("setlocal " .. opts.wrap)
-    vim.api.nvim_command "setlocal cursorline"
+    table.insert(apply_output, "")
 
-    vim.api.nvim_buf_set_lines(buf, 0, -1, false, { "Retrieving log, please wait..." })
+    local on_output = function(_, line)
+      table.insert(apply_output, line)
+
+      pcall(vim.schedule_wrap(function()
+        if vim.api.nvim_buf_is_valid(buf) then
+          vim.api.nvim_buf_set_lines(buf, 0, -1, false, apply_output)
+        end
+      end))
+    end
+
+    local job = Job:new {
+      enable_recording = true,
+      command = "terraform",
+      args = flatten(args),
+      on_stdout = on_output,
+      on_stderr = on_output,
+
+      on_exit = function(_, status)
+        if status == 0 then
+          print "Terraform apply completed!"
+        end
+      end,
+    }
+
+    if opts.mode == "sync" then
+      job:sync(opts.timeout, opts.wait_interval)
+    else
+      job:start()
+    end
+  end
+end
+
+A.init = function(opts)
+  return function(prompt_bufnr)
+    if prompt_bufnr ~= nil then
+      actions.close(prompt_bufnr)
+    end
+    local ans = vim.fn.input "[TF] are you sure you want to init infrastructure? y/n: "
+    if ans == "n" then
+      return
+    end
+
+    local apply_output = {}
+    local args = { "init", "-no-color", "-input=false" }
+    vim.api.nvim_command(opts.wincmd)
+    local buf = vim.api.nvim_get_current_buf()
+
+    nvim_buf_configure(buf, opts)
+
     table.insert(apply_output, "")
 
     local on_output = function(_, line)
